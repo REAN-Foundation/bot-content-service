ac0d9ff2f51069f2549e2a71811fac8cbd0b14c6
sushant-thotakura <sushant.thotakura@reanfoundation.org>
2026-01-14 11:46:27 -0700

    Bug Fixes for QnA Document Updates
    - Added validation for file types and mime types
    - Updated file resource update function to handle deletion of file and re uploading new file
    - Added json parse validations to support multiple types of json structures

---
+++ src/domain.types/general/file.resource/file.resource.constants.ts
@@ -0,0 +1,13 @@
+export class FileResourceConstants {
+
+    public static readonly SUPPORTED_FILE_EXTENSIONS = ['csv', 'json', 'pdf', 'txt', 'xlsx'];
+
+    public static readonly SUPPORTED_MIME_TYPES = [
+        'text/csv',
+        'application/json',
+        'application/pdf',
+        'text/plain',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'application/vnd.ms-excel'
+    ];
+}
